600e4c4bf080ada587ec8eef14f2bf6a7dccc6d2
Andrew Tolvstad <Andrew@localhost>
2017-05-09 15:04:58 -0700

    Reword and add tests for moving_slice

--- test/sentence/test_moving_slice.rb
+++ test/sentence/test_moving_slice.rb
@@ -9,20 +9,32 @@ class TestMovingSlice < Test::Unit::TestCase
   A = [1, 2, 3, 4, 5].freeze
 
   def test_to_a
-    a3 = A.moving_slice(3).to_a
-    a3_expected = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
-    assert_equal a3_expected, a3
+    result = A.moving_slice(3).to_a
+    expected = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+    assert_equal expected, result
   end
 
   def test_method
-    a3_sum = A.moving_slice(3, &:sum).to_a
-    a3_sum_expected = [(1 + 2 + 3), (2 + 3 + 4), (3 + 4 + 5)]
-    assert_equal a3_sum_expected, a3_sum
+    result = A.moving_slice(3, &:sum).to_a
+    expected = [(1 + 2 + 3), (2 + 3 + 4), (3 + 4 + 5)]
+    assert_equal expected, result
   end
 
   def test_block
-    a3_block = A.moving_slice(3) { |x, y, z| (x + y) / z.to_f }.to_a
-    a3_block_expected = [(1 + 2) / 3.0, (2 + 3) / 4.0, (3 + 4) / 5.0]
-    assert_equal a3_block_expected, a3_block
+    result = A.moving_slice(3) { |x, y, z| (x + y) / z.to_f }.to_a
+    expected = [(1 + 2) / 3.0, (2 + 3) / 4.0, (3 + 4) / 5.0]
+    assert_equal expected, result
+  end
+
+  def test_overrun_to_a
+    result = A.moving_slice(A.count + 1).to_a
+    expected = [A]
+    assert_equal expected, result
+  end
+
+  def test_overrun_method
+    result = A.moving_slice(A.count + 1, &:sum).to_a
+    expected = [A.sum]
+    assert_equal expected, result
   end
 end
